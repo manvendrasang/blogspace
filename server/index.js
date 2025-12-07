04d5c309decbe3eac08cdae4ad202526c84363bf
Manvendra <manvendrasang@gmail.com>
2025-12-07 21:25:02 +0530

    set up configurations and db connections

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyparser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy:"cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limits: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assests", express.static(path.join(__dirnamem, "public/assests")));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({ storage });
