09b7719cc7a701ade8db91c00dc717e9406b6486
Manvendra <manvendrasang@gmail.com>
2025-12-08 21:48:23 +0530

    User model and auth boilerplate

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES  */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
