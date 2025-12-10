66e4fb378aa71c68ed484432a0610fad287bcef2
Manvendra <manvendrasang@gmail.com>
2025-12-11 00:42:03 +0530

    Routes and frontend boilerplate done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
+function App() {
   return (
-    <div className="app">Hello</div>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
-export default App
+export default App;
