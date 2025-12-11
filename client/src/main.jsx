6d2c26faf30d25f3f9b9d6d47c88f4e21615313a
Manvendra <manvendrasang@gmail.com>
2025-12-11 19:44:40 +0530

    Redux state done and themes added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
+import { StrictMode, version } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import authReducer from "./state/index.js";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: (getDefaultMiddleware) => getDefaultMiddleware ({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    },
+  }),
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
