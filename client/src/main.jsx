4d06977649d0e41686e66bdbcc4d0ee5c8a258f0
Manvendra <manvendrasang@gmail.com>
2025-12-12 00:20:34 +0530

    Color, theme and styling setup done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode, version } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import authReducer from "./state/index.js";
-import { configureStore } from '@reduxjs/toolkit';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import authReducer from './state/index.js';
+
+// Redux Persist imports
 import {
   persistStore,
   persistReducer,
@@ -13,27 +17,48 @@ import {
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const persistConfig = { key: "root", storage, version: 1 };
+// -----------------------------------------------------
+// Redux Persist Configuration
+// -----------------------------------------------------
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+// Wrap reducer with persistReducer
 const persistedReducer = persistReducer(persistConfig, authReducer);
+
+// -----------------------------------------------------
+// Configure Redux Store
+// -----------------------------------------------------
 const store = configureStore({
-  reducer: (getDefaultMiddleware) => getDefaultMiddleware ({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-    },
-  }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+// Persistor instance
+const persistor = persistStore(store);
+
+// -----------------------------------------------------
+// Render App
+// -----------------------------------------------------
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
